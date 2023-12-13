18cd22d2f53e3f54a763cb2b2fbdc36c6cae36cd
kawago2 <itkawago@gmail.com>
2023-12-13 10:20:13 +0700

    [IMP] Creating an extension for setting up tableView

--- MovieTrending/Controllers/MainViewController/MainViewController.swift
+++ MovieTrending/Controllers/MainViewController/MainViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    var viewModel: MainViewModel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = MainViewModel()
+        configView()
+
+    }
+    
+    func configView() {
         self.title = "Main View"
+        self.view.backgroundColor = .red
+        
+        setupTableView()
     }
 }
---
+++ MovieTrending/ViewModel/MainViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewModel.swift
+//  MovieTrending
+//
+//  Created by Phincon on 13/12/23.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+    
+}
