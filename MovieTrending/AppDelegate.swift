8d99c777ffc976cbc45ada5fd5c00ee3209f4971
kawago2 <itkawago@gmail.com>
2023-12-13 09:44:15 +0700

    [IMP] add navigation Controller

--- MovieTrending/AppDelegate.swift
+++ MovieTrending/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = MainViewController()
+        let vc = MainViewController()
+        let navigatorController = UINavigationController(rootViewController: vc)
+        window.rootViewController = navigatorController
         window.makeKeyAndVisible()
         
         self.window = window
