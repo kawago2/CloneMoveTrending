39bd4c45eca97cc30ff6ef538448da248b1b3a01
kawago2 <itkawago@gmail.com>
2023-12-13 13:49:57 +0700

    [IMP] Add Observable Class for binding, pass data form view model to views and controllers

--- MovieTrending/Controllers/MainViewController/MainViewController.swift
+++ MovieTrending/Controllers/MainViewController/MainViewController.swift
@@ -11,16 +11,18 @@ class MainViewController: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // MARK: - Properties
     var viewModel: MainViewModel!
+    var cellDataSource: [Movies] = []
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = MainViewModel()
         configView()
-
+        bindViewModel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,5 +35,32 @@ class MainViewController: UIViewController {
         self.view.backgroundColor = .red
         
         setupTableView()
+        self.activityIndicator.hidesWhenStopped = true
+    }
+    
+    private func bindViewModel() {
+        viewModel.isLoading.bind({[weak self] isLoading in
+            guard let self = self, let isLoading = isLoading else {return}
+            
+            DispatchQueue.main.async {
+                if isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        })
+        
+        viewModel.cellDataSource.bind({[weak self] movies in
+            guard let self = self, let movies = movies else {return}
+            self.cellDataSource = movies
+            self.reloadTableView()
+        })
     }
 }
+
+            
+        
+                                      
+                                      
+                                      
--- MovieTrending/ViewModel/MainViewModel.swift
+++ MovieTrending/ViewModel/MainViewModel.swift
@@ -9,26 +9,45 @@ import Foundation
 
 class MainViewModel {
     
+    var isLoading: Observable<Bool> = Observable(false)
+    var cellDataSource: Observable<[Movies]> = Observable([])
+    var dataSource: TrendingMoviesModel?
     
     func numberOfSections() -> Int {
         1
     }
     
     func numberOfRows(in section: Int) -> Int {
-        10
+        self.dataSource?.results?.count ?? 0
     }
     
     func getData() {
-        APICaller.getTrendingMovies(completionHandler: {result in
+        if isLoading.value ?? true {
+            return
+        }
+        isLoading.value = true
+        APICaller.getTrendingMovies(completionHandler: {[weak self] result in
+            guard let self = self else {return}
+            self.isLoading.value = false    
             switch result {
             case .success(let data):
                 if let result = data.results {
                     print("Top trend count: \(result.count)")
                 }
+                self.dataSource = data
+                self.mapCellData()
             case .failure(let err):
                 print(err)
             }
             
         })
+    }
+    
+    func mapCellData() {
+        self.cellDataSource.value = self.dataSource?.results ?? []
+    }
+    
+    func getMovieTitle(_ movie: Movies) -> String {
+        return movie.title ?? movie.name ?? ""
     }
 }
